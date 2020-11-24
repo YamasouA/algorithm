64071bc908258678e211768e63639369fb365d4b
YamasouA <akiakiskyhand@gmail.com>
2020-11-24 22:22:03 +0900

    メモ化

---
+++ enshu4-6.py
@@ -0,0 +1,37 @@
+mmemo = []
+
+def func(i, w, a):
+    if i == 0:
+        if w == 0:
+            return True
+        else:
+            return False
+
+    if memo[i][w] != -1:
+        return memo[i][w]
+
+    if (func(i-1, w, a)):
+        memo[i][w] = 1
+        return memo[i][w]
+    
+    if (func(i-1, w-a[i-1], a)):
+        memo[i][w] = 1
+        return memo[i][w]
+    
+    memo[i][w] = 0
+    return memo[i][w]
+
+
+if __name__ == '__main__':
+    n, w = map(int, input().split())
+    a = []
+    for i in range(n):
+        ai = int(input())
+        a.append(ai)
+    memo = [[-1] * (w+1)] * (n+1)
+    print(memo)
+
+    if func(n, w, a):
+        print("Yes")
+    else:
+        print("No")
